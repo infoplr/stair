fcc8ce3eaed7e339ed196a68a52410419987f8f1
Céo <noah.kd@gmail.com>
2022-07-14 20:30:02 -0300

    POJIII

--- esteira steps.js
+++ esteira steps.js
@@ -1,55 +1,54 @@
 
 var dias = [dia1,dia2,dia3,dia4,dia5,dia6,dia7,dia8,dia9,dia10,dia11,dia12,dia13,dia14,dia15,dia16,dia17,dia18,dia19,dia20];
 
-dia9 = {
+dia9 = [
     cobrança
-}
+]
 
-dia10 = {
+dia10 = [
 
-}
+]
 
-dia11 = {
+dia11 = [
 
-}
+]
 
-dia12 = {
+dia12 = [
 
-}
+]
 
-dia13 = {
-    subirCurtidaDe4DiasNaPagina
-}
+dia13 = [
+    "subir Curtida De 4 Dias Na Pagina"
+]
 
-dia17 = {
-    criarOGerenciador,
-    colocarAPaginaComoPrincipal
-}
+dia17 = [
+    "criar O Gerenciador",
+    "colocar A Pagina Como Principal"
+]
 
-dia18 = {
-    postarReceitasNaPagina,
-    // cria o gerenciador
-    configurarOGerenciador,
-    criarAConta1ESobirCampanhaDeAlcancePor1Dia: "$6",
-}
+dia18 = [
+    "postar Receitas Na Pagina",
+    "configurar O Gerenciador",
+    "criar A Conta 1 E Sobir Campanha De Alcance Por 1 Dia $6",
+]
 
-dia19 = {
-    fazerOPagamento,
-    criarOutrasContas: ()=> {
-        if( criarOutrasContas ) {
-            subirCurtidasNaPaginaPor2Dias: "$12";
-            criarOutrasContas;
-            subirAlcanceNasDuasContasPor1Dia: "$7";
+dia19 = [
+    "fazer O Pagamento",
+    "criar outras contas" = { "criar Outras Contas": ()=> {
+        if( "criar Outras Contas" ) {
+            "subir Curtidas Na Pagina Por 2 Dias $12";
+            "criar Outras Contas";
+            "subir Alcance Nas Duas Contas Por 1 Dia $7";
         } else {
-            repetirAlcanceNaConta1
+            "repetir Alcance Na Conta 1"
         }
-    }
-}
+    } }
+]
 
-dia20 = {
-    sobirCurtidasNaPaginaComAsOutrasContasPor2Dias: "$12",
-}
+dia20 = [
+    "subir Curtidas Na Pagina Com As Outras Contas Por 2 Dias $12",
+]
 
-dia21 = {
-    impulsionarUmaNovaPublicaçãoNoFeedPor1Dia: "$7",
-}
+dia21 = [
+    "impulsionar Uma Nova Publicação No Feed Por 1 Dia $7",
+]
